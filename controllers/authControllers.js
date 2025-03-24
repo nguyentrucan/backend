d5f491afd4a692925c21d11250a392ac8efe6178
nguyentrucan <an.ngtruc2002@gmail.com>
2025-03-24 13:40:23 +0700

    Create Middleware And Routes Manage

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 const { createToken } = require("../utils/tokenCreate.js");
 
 class authControllers {
+  //Login
   admin_login = async (req, res) => {
     const { email, password } = req.body;
 
@@ -19,7 +20,7 @@ class authControllers {
           res.cookie("accessToken", token, {
             expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
           });
-          responseReturn(res, 200, {token, message: "Login Success" });
+          responseReturn(res, 200, { token, message: "Login Success" });
         } else {
           responseReturn(res, 401, { error: "Password Wrong" });
         }
@@ -30,6 +31,24 @@ class authControllers {
       responseReturn(res, 500, { error: error.message });
     }
   };
+  //End Login
+
+  //GetUser
+  getUser = async (req, res) => {
+    const { role, id } = req;
+
+    try {
+      if (role === "admin") {
+        const user = await adminModel.findById(id);
+        responseReturn(res, 200, { userInfo: user });
+      } else {
+        console.log("Seller Info");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  //End GetUser
 }
 
 module.exports = new authControllers();
